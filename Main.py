20cf4bdc3bd19e9eb525a27b6562ac30cd45cd7b
Jack <stanickj@bc.edum>
2025-01-31 11:40:59 -0500

    first connection of sql and python (successful)

--- Main.py
+++ Main.py
@@ -1,1 +1,39 @@
+# import sqlite to be able to work with it
+import sqlite3
+
+# establish a connection (able to make modifications and acces) and a cursor (able to execute queries)
+connect = sqlite3.connect("stock_db.db")
+cursor = connect.cursor()
+
+# delete data so not runs dont continually add
+cursor.execute("DELETE FROM test")
+connect.commit()
+
+# read the sql script file and set up the db with predfined sql code
+with open("stock_db.sql", "r") as sql_file:
+    sql_data = sql_file.read()
+# have the cursor execute the prewritten script and then connect and update to the database
+cursor.executescript(sql_data)
+connect.commit
+
+# select and store initial data from database
+cursor.execute("SELECT * FROM test")
+initial = cursor.fetchall()
+
+# execute a change that modifies the table (insert row) so needs to be committed to data base
+cursor.execute("INSERT OR IGNORE INTO test (name) VALUES ('Kate')")
+connect.commit()
+
+# store the next table
+cursor.execute("SELECT * FROM test")
+second = cursor.fetchall()
+
+print(initial)
+print(second)
+
+# always close connection
+connect.close()
+
+
+
 
--- stock_db.sql
+++ stock_db.sql
@@ -1,1 +1,10 @@
+-- need to only create if it doesnt exist or it will try and make new table every time file is ran
+CREATE TABLE IF NOT EXISTS test (
+    name TEXT
+);
+
+-- just inserting values
+INSERT OR IGNORE INTO test (name)
+VALUES ('Jack');
+
 
