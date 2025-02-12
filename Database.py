bda8dc5ecfba13408b96c6a60eaa9af6735afaff
Jack <stanickj@bc.edum>
2025-02-11 23:43:53 -0500

    work 2/3 on the one company program

--- Database.py
+++ Database.py
@@ -69,6 +69,47 @@ class Database :
             else : 
                 print("Please enter valid input.")
 
+    # general function for one company
+    def oneComp (self) :
+       # ask the user for which company they would like to operate on
+       self.print_companies()
+       # oppurtunity to validate input
+       company = int(input("Please enter the company id: ")) 
+
+       # take in the user action option; can be updated to add input validation
+       self.print_options_one()
+       choice = int(input("Selection: "))
+
+       # after they enter which task, call an appropriate method on the task to complete (this is like a switch in java)
+       match choice :
+            case 1 : 
+               # take in the year and then call method; spot to validate input
+               year = int(input("What year would you like to see (2015 - 2024): "))
+               self.prices_one_year(company, year)
+            case 2 :
+               # call method to see prices for every year
+               self.prices_one_all(company)
+            case 3 :
+               # call method to see average stock price
+               self.avg_one(company)
+            case 4 :
+               # call method to see maximum stock price
+               self.max_one(company)
+            case 5 :
+               # call method to see minimum stock price
+               self.min_one(company)
+            case 6 :
+               # call method to see standard deviation of stock prices
+               self.std_one(company)
+            case 7 :
+               # call method to see the yearly increase/decrease
+               self.inc_one(company)
+            case 8 :
+               # call method to rank highest price years
+               self.rank_one(company)
+            case _ :
+               print("Invalid. Ending selection process.")
+               
     def print_companies (self) :
         # query to just have the company id and the company name
         query = """
@@ -80,7 +121,53 @@ class Database :
         self.cursor.execute(query)
         self.print_pretty_all()
     
-    # takes in a company id and returns the tuple with the name, ticker, and avg price
+    # prints the different options that method one can do
+    def print_options_one (self) :
+        print("\nPlease enter the number that corresponds to what you would like to do: ")
+        print("1. See stock prices for one year.")
+        print("2. See stock prices for all years.")
+        print("3. See average stock price.")
+        print("4. See maximum stock price.")
+        print("5. See minimum stock price.")
+        print("6. See standard deviation of stock prices.")
+        print("7. See yearly increase/decrease of stock prices.")
+        print("8. See rank of highest price years.")
+
+    # takes in the company and year and prints and ticker, year, and rounded price for that year
+    def prices_one_year (self, company_id, year) :
+        # string query, just selecting entry with matching year and company (see avg_one for the placeholder explanation)
+        query = """
+        SELECT companies.ticker, 
+            shares.year,
+            ROUND(shares.price, 2)
+        FROM shares
+        JOIN companies
+            ON shares.company_id = companies.id
+        WHERE shares.company_id = ?
+            AND shares.year = ?
+        """
+        # execute query with placeholders and print
+        self.cursor.execute(query, (company_id, year))
+        self.print_pretty_all()
+
+    # selects and prints the ticker, year, and rounded price for all years of one company
+    def prices_one_all (self, company_id) :
+        # string query to select all shares of a given company
+        query = """
+        SELECT companies.ticker,
+            shares.year,
+            ROUND(shares.price, 2)
+        FROM shares
+        JOIN companies
+            ON shares.company_id = companies.id
+        WHERE shares.company_id = ?
+        """
+        # execute and print
+        self.cursor.execute(query, (company_id,))
+        self.print_pretty_all()
+        
+    
+    # takes in a company id and prints the ticker and rounded price
     def avg_one (self, company_id) :
         # execute the average command using a place holder (?) for the python company parameter
         # join the tables for proper output on primary/foreign key and group by the company id to select the average
@@ -92,21 +179,50 @@ class Database :
             ON shares.company_id = companies.id
         WHERE shares.company_id = ?
         GROUP BY companies.id"""
-        # execute the query, no need to return since the cursor instance variable stores the desired results
+        # execute the query and print, no need to return since the cursor instance variable stores the desired results
         self.cursor.execute(query, (company_id,))
-        
-    # general function for one company
-    def oneComp (self) :
-       # ask the user for which company they would like to operate on
-       self.print_companies()
-       # oppurtunity to validate input
-       choice = int(input("Please enter the company id: "))
+        self.print_pretty_all()
 
-       # call the average company with the desired company (will be stored to the cursor, and then the print method will print the cursor storage)
-       # WILL HAVE LOGIC ASKING FOR WHAT METHOD TO CALL IN FUTURE
-       self.avg_one(choice)
-       self.print_pretty_all()
-    
+    # selects and prints the ticker, year, and maximum price of a company
+    def max_one (self, company_id) :
+        # query to select max, group by ticker (company)
+        query = """
+        SELECT companies.ticker,
+            shares.year,
+            MAX(shares.price)
+        FROM shares
+        JOIN companies
+            ON shares.company_id = companies.id
+        WHERE shares.company_id = ?
+        GROUP BY 1
+        """
+        self.cursor.execute(query, (company_id,))
+        self.print_pretty_all()
+
+    # same code as max just a different aggregate function
+    def min_one (self, company_id) :
+        query = """
+        SELECT companies.ticker,
+            shares.year,
+            MIN(shares.price)
+        FROM shares
+        JOIN companies
+            ON shares.company_id = companies.id
+        WHERE shares.company_id = ?
+        GROUP BY 1
+        """
+        self.cursor.execute(query, (company_id,))
+        self.print_pretty_all()
+
+    def std_one (self, company_id) :
+        print("std one")
+
+    def inc_one (self, company_id) :
+        print("inc one")
+
+    def rank_one (self, company_id) :
+        print("rank one")
+
     # general function for two companies
     def twoComp (self) :
         print("Two Companies")
